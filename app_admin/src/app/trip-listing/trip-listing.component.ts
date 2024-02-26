4ab55388c55e567c2244ec9d56f4242a44299344
Makayla Anderson-Tucker <makayla.anderson.tucker@gmail.com>
2024-02-25 20:06:44 -0800

    Completed module 6 assignment.

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,18 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { trips } from '../data/trips';
+import { Router } from '@angular/router';
+// import { trips } from '../data/trips';
+import { TripDataService } from 'src/app/services/trip-data.service';
+import { Trip } from 'src/app/models/trip';
 
 @Component({
   selector: 'app-trip-listing',
   templateUrl: './trip-listing.component.html',
-  styleUrls: ['./trip-listing.component.css']
+  styleUrls: ['./trip-listing.component.css'],
+  providers: [TripDataService]
 })
+
 export class TripListingComponent implements OnInit {
+  // trips: Array<any> = trips;
+  trips: Trip[];
 
-  trips: Array<any> = trips;
+  message: string;
   
-  constructor() { }
+  constructor(
+    private tripDataService: TripDataService,
+    private router: Router
+    ) { }
 
-  ngOnInit() {
+    private addTrip(): void {
+      this.router.navigate(['add-trip']);
+    }
+
+    private getTrips(): void {
+      console.log('Inside TripListingComponent#getTrips');
+      this.message = 'Searching for trips';
+      this.tripDataService
+        .getTrips()
+        .then(foundTrips => {
+        this.message = foundTrips.length > 0 ? ''
+        : 'No trips found';
+      this.trips = foundTrips;
+ });
   }
-
+ngOnInit(): void {
+ this.getTrips();
 }
+}
+
