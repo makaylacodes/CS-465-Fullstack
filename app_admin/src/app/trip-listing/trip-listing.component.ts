99b54ffe8f322d9078f72480407b0e163c7f5b04
Makayla Anderson-Tucker <makayla.anderson.tucker@gmail.com>
2024-02-26 10:02:02 -0800

    Completed module 7 assignment.

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 // import { trips } from '../data/trips';
 import { TripDataService } from 'src/app/services/trip-data.service';
 import { Trip } from 'src/app/models/trip';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-trip-listing',
@@ -19,12 +20,16 @@ export class TripListingComponent implements OnInit {
   
   constructor(
     private tripDataService: TripDataService,
-    private router: Router
+    private router: Router,
+    private authenticationService: AuthenticationService
     ) { }
 
     private addTrip(): void {
       this.router.navigate(['add-trip']);
     }
+    public isLoggedIn(): boolean {
+      return this.authenticationService.isLoggedIn();
+     }
 
     private getTrips(): void {
       console.log('Inside TripListingComponent#getTrips');
